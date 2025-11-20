b675231195e0a4e1a31c17d259ad65c8752f09a9
PrestigeSucc <jobsterq2006@gmail.com>
2025-11-20 22:32:38 +0200

    yes

--- modules/home/apps/programs.nix
+++ modules/home/apps/programs.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }: {
   
    home.packages = with pkgs; [
+      zim
       anydesk
       libreoffice
       nix-search-tv
       mpvScripts.webtorrent-mpv-hook
       nh
+      openrussian-cli
     ];
 }
